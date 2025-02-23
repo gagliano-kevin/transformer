fdf385148fd134644dde1f1626efc6227b2528f4
gagliano-kevin <gagliano.kevin.97@gmail.com>
2025-02-23 18:43:49 +0100

    forward in transformer class and optimizer initialization method

--- transormer.py
+++ transormer.py
@@ -92,17 +92,17 @@ Transformer model
 class transformer(nn.Module):
     def __init__(self, config: transformerConfig):
         super(transformer, self).__init__()
-        self.config = config                                                                                    # Configuration for the transformer   
+        self.config = config                                                                                            # Configuration for the transformer   
 
-        self.token_embedding = nn.Embedding(config.vocab_size, config.embedding_dim)                            # Token embedding layer
-        self.positional_embedding = nn.Parameter(torch.randn(config.max_seq_len, config.embedding_dim))         # Positional embedding layer
-        self.transformer_blocks = nn.ModuleList([transformerBlock(config) for _ in range(config.num_layers)])   # List of transformer blocks
-        self.norm_layer = nn.LayerNorm(config.embedding_dim)                                                    # Normalization layer
-        self.output_layer = nn.Linear(config.embedding_dim, config.vocab_size, bias=False)                      # Output layer
+        self.token_embedding_layer = nn.Embedding(config.vocab_size, config.embedding_dim)                              # Token embedding layer
+        self.positional_embedding_layer = nn.Parameter(torch.randn(config.max_seq_len, config.embedding_dim))           # Positional embedding layer
+        self.transformer_blocks = nn.ModuleList([transformerBlock(config) for _ in range(config.num_layers)])           # List of transformer blocks
+        self.norm_layer = nn.LayerNorm(config.embedding_dim)                                                            # Normalization layer
+        self.output_layer = nn.Linear(config.embedding_dim, config.vocab_size, bias=False)                              # Output layer
 
-        self.token_embedding.weight = self.output_layer.weight                                                  # Tie the weights of the token embedding and output layer
+        self.token_embedding.weight = self.output_layer.weight                                                          # Tie the weights of the token embedding and output layer
     
-        self.apply(self.init_weights)                                                                           # Initialize the weights of the model
+        self.apply(self.init_weights)                                                                                   # Initialize the weights of the model
     
     def init_weights(self, module):
         if isinstance(module, nn.Linear):
@@ -120,12 +120,43 @@ class transformer(nn.Module):
             torch.nn.init.constant_(module.weight, 1)                                         # Initialize the weights of the layer normalization layer with constant
         """
 
-    def forward(self, x):
+    def forward(self, x, targets=None):
         batch_size, seq_len = x.size()                                                          # Get the batch size and sequence length
         assert seq_len <= self.config.max_seq_len, f"Input sequence length = {seq_len} exceeds the maximum sequence length = {self.config.max_seq_len}"
 
-        #---
-        x = self.token_embedding(x) + self.positional_embedding[:seq_len]                       # Add token and positional embeddings
+        positions = torch.arange(seq_len, device=x.device).expand(batch_size, seq_len)          # Generate positions tensor
+        positional_embedding = self.positional_embedding_layer(positions)                       # Get positional embeddings for the input sequence (batch_size, seq_len, embedding_dim)
+        token_embedding = self.token_embedding_layer(x)                                         # Get token embeddings for the input sequence (batch_size, seq_len, embedding_dim)
+        x = token_embedding + positional_embedding                                              # Add token and positional embeddings 
         for block in self.transformer_blocks:
             x = block(x)                                                                        # Pass through the transformer blocks
         x = self.norm_layer(x)                                                                  # Apply normalization
+        x = self.output_layer(x)                                                                # Apply output layer
+        loss = None                                                                             # Initialize loss to None
+        if targets is not None:
+            loss = F.cross_entropy(x.view(-1, x.size(-1)), targets.view(-1))                    # Calculate the cross entropy loss
+        return x, loss                                                                          # Return the output(logits) and loss
+
+    def init_optimizers(self, weight_decay, learning_rate, device):
+        parameter_dict = {param_name : param for param_name, param in self.named_parameters() if param.requires_grad}   # Get the parameters that require gradients
+        weight_decay_parameters = [param for name, param in parameter_dict.items() if param.dim >= 2]                   # Get the parameters that require weight decay (only for bidiemensional tensors)
+        no_weight_decay_parameters = [param for name, param in parameter_dict.items() if param.dim < 2]                 # Get the parameters that do not require weight decay
+        optim_param_group = [
+            {"params": weight_decay_parameters, "weight_decay": weight_decay},
+            {"params": no_weight_decay_parameters, "weight_decay": 0.0}
+        ]
+        decay_param_tensor_num = len(weight_decay_parameters)                                                           # Number of tensors that require weight decay
+        decay_param_num = sum(param.numel() for param in weight_decay_parameters)                                       # Number of parameters that require weight decay
+        no_decay_param_tensor_num = len(no_weight_decay_parameters)                                                     # Number of tensors that do not require weight decay
+        no_decay_param_num = sum(param.numel() for param in no_weight_decay_parameters)                                 # Number of parameters that do not require weight decay
+        
+        if master_process:
+            print(f"number of decayed parameter tensors: {decay_param_tensor_num}, with {decay_param_num} parameters")
+            print(f"number of non-decayed parameter tensors: {no_decay_param_tensor_num}, with {no_decay_param_num} parameters")
+        
+        fused_available = 'fused' in inspect.signature(torch.optim.AdamW).parameters
+        use_fused = fused_available and device == "cuda"
+        if master_process:
+            print(f"using fused AdamW: {use_fused}")
+        optimizer = torch.optim.AdamW(optim_param_group, lr=learning_rate, betas=(0.9, 0.95), eps=1e-8, fused=use_fused)
+        return optimizer
