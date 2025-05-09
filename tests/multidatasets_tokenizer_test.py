ef208a97276e305f8c2936cbb0f042676b5f5943
gagliano-kevin <gagliano.kevin.97@gmail.com>
2025-05-09 19:41:41 +0200

    test on multidatasets tokenizer

--- tests/multidatasets_tokenizer_test.py
+++ tests/multidatasets_tokenizer_test.py
@@ -36,44 +36,9 @@ tokenizer.save_model("multidatasets_tokenizer")
 tokenizer = BPE_tokenizer(vocab_size=260)
 tokenizer.tokenizer_params_dir = "./multidatasets_tokenizer_params"
 tokenizer.load_model("multidatasets_tokenizer")
+text_len = len(text)
+print(f"\n\nlen of thext: {text_len}\n\n")
 
-
-test_string = """
-Mirabilia è uno studio che si occupa di Realizzazione Siti web,
-Comunicazione Grafica, logo design e web Marketing con base a Palermo
-per informazioni visitate il sito web www.mirabiliaweb.net o
-contattate info@mirabiliaweb.net
-1. DIARIO DI JONATHAN HARKER
-(Stenografato).
-3 maggio, Bistrita. Lasciata Monaco alle 20,35 dei primo maggio, giunto a
-Vienna il mattino dopo presto; saremmo dovuti arrivare alle 6,46, ma il treno
-aveva un'ora di ritardo. Stando al poco che ho potuto vederne dal treno e
-percorrendone brevemente le strade, Budapest mi sembra una bellissima
-città.
-Non ho osato allontanarmi troppo dalla stazione, poiché, giunti in ritardo,
-saremmo però ripartiti quanto più possibile in orario. Ne ho ricavato
-l'impressione che, abbandonato l'Occidente, stessimo entrando nell'Oriente, e
-infatti anche il più occidentale degli splendidi ponti sul Danubio, che qui è
-maestosamente ampio e profondo, ci richiamava alle tradizioni della
-dominazione turca.
-Siamo partiti quasi in perfetto orario, e siamo giunti a buio fatto a
-Klausenburg, dove ho pernottato all'albergo Royale. A pranzo, o meglio a
-cena,
-mi è stato servito pollo cucinato con pepe rosso, buonissimo, ma che mi ha
-messo una gran sete (Ric.: farsi dare la ricetta per Mina). Ne ho parlato con il
-cameriere, il quale mi ha spiegato che si chiama "paprika hendl", e che,
-essendo
-un piatto nazionale, avrei potuto gustarlo ovunque nei Carpazi. Ho trovato
-assai
-utile la mia infarinatura di tedesco; in verità, non so come potrei cavarmela
-senza di essa.
-Poiché a Londra avevo avuto un po' di tempo a disposizione, mi ero recato
-al British Museum, nella cui biblioteca avevo compulsato libri e mappe sulla
-Transilvania: mi era balenata l'idea che avrebbe potuto essermi utile qualche
-informazione sul paese, visto che dovevo entrare in rapporti con un nobile del
-luogo. Ho scoperto che il distretto da questi indicato si trova ai limiti orientali
-del paese, proprio alla convergenza di tre stati, Transilvania, Moldavia e
-"""
-
-print(tokenizer.encode(test_string))
-print(tokenizer.decode(tokenizer.encode(test_string)))
+# Test on 1/10 of the text
+#print(tokenizer.encode(text[:text_len//10]))
+#print(tokenizer.decode(tokenizer.encode(text[:text_len//10])))
